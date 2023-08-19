da10e5eba32f2dc30b09cc8c6bea32dd98e2fdf2
Nathanael <Nathanael.carauna@gmail.com>
2023-08-19 09:23:33 -0300

    feature: Create cliente repository,  update, delete and nameSearch. Test all methods

--- src/main/java/io/gihub/nathanaelcarauna/VendasApplication.java
+++ src/main/java/io/gihub/nathanaelcarauna/VendasApplication.java
@@ -15,15 +15,39 @@ import java.util.List;
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes){
+    public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
+            System.out.println("Salvando clientes");
             clientes.salvar(new Cliente("Nathanael"));
             clientes.salvar(new Cliente("Symcia"));
 
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " Atualizado");
+                clientes.atualizar(c);
+            });
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("nael").forEach(System.out::println);
+
+//            System.out.println("Deletando cliente");
+//            clientes.obterTodos().forEach(c -> clientes.deletar(c));
+
+            todosClientes = clientes.obterTodos();
+            if (todosClientes.isEmpty()) {
+                System.out.println("Nenhum cliente encontrado");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
+
         };
     }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
--- src/main/java/io/gihub/nathanaelcarauna/domain/repository/Clientes.java
+++ src/main/java/io/gihub/nathanaelcarauna/domain/repository/Clientes.java
@@ -15,6 +15,10 @@ public class Clientes {
 
     private static final String SELECT_ALL = "SELECT * FROM CLIENTE;";
     private static final String INSERT = "INSERT INTO CLIENTE (nome) VALUES (?);";
+    private static final String UPDATE = "UPDATE CLIENTE SET NOME = ? WHERE ID = ?;";
+    private static final String DELETE = "DELETE FROM CLIENTE WHERE ID = ?;";
+    private static final String SELECT_POR_NOME = "SELECT * FROM CLIENTE WHERE NOME like ?;";
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -23,14 +27,35 @@ public class Clientes {
         return cliente;
     }
 
-    public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+    public Cliente atualizar(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()});
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente) {
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id) {
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> buscarPorNome(String nome) {
+        return jdbcTemplate.query(SELECT_POR_NOME, new Object[]{"%" + nome + "%"}, getClienteRowMapper());
+    }
+
+    public List<Cliente> obterTodos() {
+        return jdbcTemplate.query(SELECT_ALL, getClienteRowMapper());
+    }
+
+    private static RowMapper<Cliente> getClienteRowMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 int id = resultSet.getInt("id");
                 String nome = resultSet.getString("nome");
                 return new Cliente(id, nome);
             }
-        });
+        };
     }
 }
