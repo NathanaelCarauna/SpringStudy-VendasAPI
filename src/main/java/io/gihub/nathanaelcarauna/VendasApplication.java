1e4af5e1a1a5c8de16bd631906a09518633d222b
Nathanael <Nathanael.carauna@gmail.com>
2023-08-19 08:58:00 -0300

    feature: Create cliente repository,  insert and list methods

--- src/main/java/io/gihub/nathanaelcarauna/VendasApplication.java
+++ src/main/java/io/gihub/nathanaelcarauna/VendasApplication.java
@@ -1,11 +1,29 @@
 package io.gihub.nathanaelcarauna;
 
 
+import io.gihub.nathanaelcarauna.domain.entity.Cliente;
+import io.gihub.nathanaelcarauna.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente("Nathanael"));
+            clientes.salvar(new Cliente("Symcia"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
