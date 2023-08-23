6c591235e625f521e4e222bc6d70400af10c638c
Nathanael <Nathanael.carauna@gmail.com>
2023-08-22 21:59:32 -0300

    feature: Add RegraDeNegocio Exception Handler

---
+++ src/main/java/io/gihub/nathanaelcarauna/api/ApiErrors.java
@@ -0,0 +1,17 @@
+package io.gihub.nathanaelcarauna.api;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErrors {
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(String memsagem) {
+        this.errors = Arrays.asList(memsagem);
+    }
+}
---
+++ src/main/java/io/gihub/nathanaelcarauna/api/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,21 @@
+package io.gihub.nathanaelcarauna.api.controller;
+
+import io.gihub.nathanaelcarauna.api.ApiErrors;
+import io.gihub.nathanaelcarauna.exception.RegraNegocioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex){
+        String message = ex.getMessage();
+        return new ApiErrors(message);
+    }
+}
